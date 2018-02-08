1d0a45cd684d0556d2ac1f1117e80965d2ec0e5f
Atid Srisukhantapuek <atid.sr@ku.th>
2018-02-08 08:32:14 +0700

    update Javadoc

--- src/coinpurse/Valuable.java
+++ src/coinpurse/Valuable.java
@@ -2,12 +2,17 @@ package coinpurse;
 
 /**
  * An interface for objects having a monetary value and currency 
+ * @author Atid Srisukhantapuek
  */
 public interface Valuable {
 	/**
-	 * Get the monetary value of this object, in its own currency
+	 * Get the value of this object
 	 * @return the value of this object
 	 */
 	public double getValue();
+	/**
+	 * Get the monetary value of this object
+	 * @return the currency of this object
+	 */
 	public String getCurrency();
 }
