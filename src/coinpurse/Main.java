899c7423d350e74a993379d09b01f10b40bfeb81
Atid Srisukhantapuek <atid.sr@ku.th>
2018-02-18 15:02:35 +0700

    Update problem 5

--- src/coinpurse/Main.java
+++ src/coinpurse/Main.java
@@ -1,5 +1,7 @@
 package coinpurse;
- 
+
+import java.util.ResourceBundle;
+
 /**
  * A main class to create objects and connect objects together.
  * The user interface needs a reference to coin purse.
@@ -12,6 +14,21 @@ public class Main {
      * @param args not used
      */
     public static void main( String[] args ) {
+    	ResourceBundle bundle = ResourceBundle.getBundle("purse");
+    	String factoryclass = bundle.getString("moneyfactory");
+    	
+    	MoneyFactory factory = null;
+    	try {
+			factory = (MoneyFactory)Class.forName(factoryclass).newInstance();
+		} catch (ClassCastException cce) {
+			System.out.println(factoryclass+"is not type MoneyFactory");
+		} catch(Exception ex) {
+			System.out.println("Error creating Money Factory"+ex.getMessage());
+		}
+    	
+    	if (factory == null) System.exit(1);
+    	
+    	MoneyFactory.setFactory(factory);
         // 1. create a Purse
     	Purse purse = new Purse(10);
         // 2. create a ConsoleDialog with a reference to the Purse object
